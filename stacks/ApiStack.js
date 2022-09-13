704af1b4cf2a9a5f83cd486476b60f5936dde91d
Dondre Lockett <dondre@Dondres-MacBook-Air.local>
2022-09-13 11:50:37 -0500

    Adding a billing api

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -11,6 +11,7 @@ export function ApiStack({ stack, app }) {
                 permissions: [table],
                 environment: {
                     TABLE_NAME: table.tableName,
+                    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
                 },
             },
         },
@@ -19,7 +20,8 @@ export function ApiStack({ stack, app }) {
             "GET /notes/{id}": "functions/get.main",
             "GET /notes": "functions/list.main",
             "PUT /notes/{id}": "functions/update.main",
-            "DELETE /notes/{id}": "functions/delete.main"
+            "DELETE /notes/{id}": "functions/delete.main",
+            "POST /billing": "functions/billing.main",
         },
     });
     // Show the API endpoint in the output
